6a4820a8e6690cbc99b963679bf63861887f9fde
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-07-30 20:33:45 +0500

    Python_project_random_string_generator

---
+++ python_projects/python_random_string_generator/python.py
@@ -0,0 +1,19 @@
+import string
+import random
+from pyperclip import copy 
+
+def generate_random_string() -> None:
+   try:
+    length:int = int(input("Enter the length of the string(0-1000000) you want to generate...  "))
+    if length > 0 and length <= 1000000:
+        random_string:str = ''.join(random.choice(string.ascii_letters + '\n') for _ in range(length))
+        print(random_string)
+        
+        copy(random_string)
+        print('\nCopied to clipboard paste it anywhere!')
+    else:    
+        print('Please enter a value between "0" and "1000000"!')
+   except ValueError:
+        print('Please enter a numeric value between "0" and "1000000"!')
+
+generate_random_string()
