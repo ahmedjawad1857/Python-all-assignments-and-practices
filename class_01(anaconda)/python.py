e833b413618997fac81a85f8326b81e067e1cfe8
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-07-30 19:57:09 +0500

    Class_01(anaconda) python

---
+++ class_01(anaconda)/python.py
@@ -0,0 +1,10 @@
+import sys
+
+print(sys.path)
+name : str = "Pakistan"
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
+
+
